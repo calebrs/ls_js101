da8d3c249b2a9ed1cb154aee8bcdb5671db07c20
Caleb Smith <smithcaleb042@gmail.com>
2020-09-21 20:16:12 +0000

    20200921 excercises and lesson 2 commit

---
+++ excercises/temp.js
@@ -0,0 +1,7 @@
+function stringToSignedInteger(string) {
+  return string - 0;
+}
+
+console.log(stringToSignedInteger("4321") === 4321); // logs true
+console.log(stringToSignedInteger("-570") === -570); // logs true
+console.log(stringToSignedInteger("+100") === 100); 
---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,26 @@
+
+//Returns the sum of two numbers
+//START
+// get input for first number 
+//get input for second number
+//perform the addition on the two numbers 
+//return the answer to the user
+
+
+//a function that takes an array of strings, and returns a string that is all those strings concatenated together
+//START
+//function that gets the array of strings
+//join all of the strings in the array 
+//return the answer to the user.
+// END
+
+
+//a function that takes an array of integers, and returns a new array with every other element
+//START
+//Make funtion that gets the input array of integers
+// SET a new array that will be output
+//iterate through the input array
+//if the index of the array is even, push it to the new array
+//otherwise continue iterating over the array.
+//return the new array that contains every other value of the array.
+//END
