db02877532d55fa9e108290a5bcf9693758ed35c
Caleb Smith <smithcaleb042@gmail.com>
2020-09-28 22:18:43 +0000

    lesson 3 commit 20200928

---
+++ lesson_3/temp.js
@@ -0,0 +1,5 @@
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+
+let newSentance = munstersDescription[0].toUpperCase() + munstersDescription.slice(1).toLowerCase();
+
+console.log(newSentance);
