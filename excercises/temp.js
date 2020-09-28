2538e42e7e976c58891a69853f80a13156bf6071
Caleb Smith <smithcaleb042@gmail.com>
2020-09-28 21:48:00 +0000

    excercises commit 20200928

--- excercises/temp.js
+++ excercises/temp.js
@@ -1,18 +1,45 @@
-function century(year) {
-  let centuryNumber = Math.ceil(year / 100);
-  let centuryString = String(centuryNumber);
-  let lastTwo = centuryString[centuryString.length - 2] + centuryString[centuryString.length - 1];
+/*
+input - sentance or word that's a string, words are seperated by spaces.
+words have at least one letter and the whole string will have at least one word
+output - sentance or a word that's a string, words are seperated by spaces
+sentance goes in and then a sentance where the first and last letters of each word
+are swapped.
+input is string, arrays
+output is string
+read in the string as an array
+take each word and swap the first and last letter
+    break string into array
+    set first letter string
+    set last letter string
+    reassign first index
+    reassign last index
+    join the array back
+join the array by ' '
 
-  if (lastTwo === '11' || lastTwo === '12' || lastTwo === '13') {
-    return centuryString + 'th';
-  }
-  
-  switch (centuryString[centuryString.length - 1]) {
-    case '1': return centuryString + 'st';
-    case '2': return centuryString + 'nd';
-    case '3': return centuryString + 'rd';
-    default: return centuryString + 'th';
-  }
+
+*/
+function swap(sentance) {
+  let sentanceArray = sentance.split(' ');
+  let swappedSentanceArray = sentanceArray.map(word => swapFirstAndLastLetter(word));
+  return swappedSentanceArray.join(' ');
 }
 
-console.log(century(2000));
+function swapFirstAndLastLetter(word) {
+  
+  if (word.length === 1) return word;
+  
+  let wordArray = word.split('');
+  let firstLetter = wordArray[0];
+  let lastLetter = wordArray[wordArray.length - 1];
+  
+  wordArray[0] = lastLetter;
+  wordArray[wordArray.length - 1] = firstLetter;
+  
+  return wordArray.join('');
+}
+
+
+
+console.log(swap('Oh what a wonderful day it is'));
+console.log(swap('Abcde'));
+console.log(swap('a'));
