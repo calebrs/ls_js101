bb5bcdaf82bbc9f143c22df29d70dbeb4ddfda47
Caleb Smith <smithcaleb042@gmail.com>
2020-10-23 20:24:09 +0000

    meduim excercises commit

--- lesson_6/temp.js
+++ lesson_6/temp.js
@@ -1,16 +1,6 @@
-function joinOr(array, divider = ', ', statement = 'or') {
-  switch (array.length) {
-    case 1: 
-      return array
-    case 2:
-      return `${array[0]} ${statement} ${array[1]}`
-    default:
-      return `${array.slice(0, array.length - 1).join(divider)}${divider}${statement} ${array[array.length - 1]}`
-      // code
-  }
+let func = function(arg) {
+  return arg + arg;
 }
+  
 
-console.log(joinOr([1, 2]));
-console.log(joinOr([1, 2, 3]));
-console.log(joinOr([1, 2, 3], '; '));
-console.log(joinOr([1, 2, 3], ', ', 'and'));
+console.log(func(20))
